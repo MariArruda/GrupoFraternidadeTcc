ab0d926dc502f101b80b3dfbce735be40d206e89
Fabricio de Paula Oliveira <fabricio_paula@hotmail.com>
2018-07-21 13:15:33 -0300

    Inclusão de mais validações no jquery e continuação da inserção de item no estoque

--- paginas/farmacia/js/farmacia.js
+++ paginas/farmacia/js/farmacia.js
@@ -15,61 +15,74 @@ $("#ddlMedicamento").change(function(){
    
 });
 
-$("#btnCadastrar").click(function(){
+$("#btnCadastrar").click(function(e){
  
     if($("#ddlTipo").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar o tipo de medicamento!");
+        e.preventDefault();
     }
 
     if($("#ddlMedicamento").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar o medicamento!");
+        e.preventDefault();
     }
 
     if($("#ddlViaAdministracao").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar a vida de administração!");
+        e.preventDefault();
     }
 
     if($("#ddlUnidadeMedida").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar a unidade de medida!");
+        e.preventDefault();
     }
 
     if($("#numberQtde").val() == "" || $("#numberQtde").val() == 0)
     {
         alert("É preciso informar a quantidade de estoque crítico e a quantidade deve ser maior que zero!");
+        e.preventDefault();
     }
+
+    $("#modal-cadastro-sucesso").click();
 
 
 });
 
-$("#btnEntradaEstoque").click(function(){
+$("#btnEntradaEstoque").click(function(e){
  
     if($("#ddlTipo").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar o tipo de medicamento!");
+        e.preventDefault();
+        
     }
 
     if($("#ddlMedicamento").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar o medicamento!");
+        e.preventDefault();
     }
 
     if($("#ddlViaAdministracao").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar a vida de administração!");
+        e.preventDefault();
     }
 
     if($("#ddlUnidadeMedida").find(":selected").text() == "Selecione...")
     {
         alert("É preciso informar a unidade de medida!");
+        e.preventDefault();
     }
 
     if($("#numberQtde").val() == "" || $("#numberQtde").val() == 0)
     {
         alert("É preciso informar a quantidade de estoque crítico e a quantidade deve ser maior que zero!");
+        e.preventDefault();
     }
 
 
